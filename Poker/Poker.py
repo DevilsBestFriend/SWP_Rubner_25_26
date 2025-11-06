af4c91a5884141874e4fe25bb691f4bf41d93f8e
Peter Quickner <peteryt690@gmail.com>
2025-11-06 20:05:48 +0100

    deal hand, change combinations

--- Poker/Poker.py
+++ Poker/Poker.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class Color(Enum):
     K = "Kreuz ♣"
@@ -22,15 +23,15 @@ class Number(Enum):
     ACE = 14
 
 class Combinations(Enum):
-    HIGHCARD = 1
-    PAIR = 2
-    TWOPAIR = 3
-    THREEOFAKIND = 4
-    STRAIGHT = 5
-    FLUSH = 6
-    FULLHOUSE = 7
-    FOUROFAKIND = 9
-    STRAIGHTFLUSH = 10
+    HIGHCARD = [[1,0,0,0,0],0]
+    PAIR = [[1,1,0,0,0],1]
+    TWOPAIR = [[1,1,2,2,0],2]
+    THREEOFAKIND = [[1,1,1,0,0],3]
+    STRAIGHT = [[1,2,3,4,5],4]
+    FLUSH = [[2,2,2,2,2],5]
+    FULLHOUSE = [[1,1,1,2,2],6]
+    FOUROFAKIND = [[1,1,1,1,0],7]
+    STRAIGHTFLUSH = [[1,2,3,4,5],8]
 
 class Card():
     def __init__(self, color: Color, number: Number):
@@ -40,15 +41,20 @@ class Card():
     def __str__(self):
         # show enum name and value for Number, and the symbol/text for Color
         return str(self.number.value)+" "+str(self.color.value)
+    
+def dealhand(cards):
+    hand = [random.sample(cards,5)] # 5 cards to the player
+    hand = sorted(hand[0], key=lambda x: x.number.value) # sort by the value of the number enum
+    return hand
+    
 
 def main ():
     cards = []
-    for color in Color:
+    for color in Color: # Generate a list of all cards
         for number in Number:
             cards.append(Card(color, number))
-    
-    for card in cards:
-        print(card)
+
+
 
 
 if __name__ == "__main__":
