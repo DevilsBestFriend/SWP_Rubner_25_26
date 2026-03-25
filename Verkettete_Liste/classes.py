7719354ca111609e6792155d4d7bbcb37291e2f6
Peter Quickner <pquickner@tsn.at>
2026-03-25 16:26:50 +0100

    insert, delete, pop, __len__, first, last

--- Verkettete_Liste/classes.py
+++ Verkettete_Liste/classes.py
@@ -1,19 +1,22 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next = None):
         self.data = data
-        self.next = None
+        self.next = next
 
     def __str__(self):
-        return "Data: " + str(self.data) + "; "
+        return str(self.data)
+    
+    def __repr__(self):
+        return "Data= " + self.data + " Next= " + self.next
 
 
 class LinkedList:
 
     def __init__(self, data):
-        self.first = Node(data=data)
+        self._first_node = Node(data=data)
 
     def __iter__(self):
-        self.current = self.first
+        self.current = self._first_node
         return self
 
     def __next__(self):
@@ -24,37 +27,64 @@ class LinkedList:
         self.current = self.current.next
         return data
 
-    def apendieren(self, data):
-        next = self.first
+    def __add__(self, data):
+        next = self._first_node
         while next.next != None:
             next = next.next
         next.next = Node(data=data)
         
-    def insertieren():
-        pass
+    def insertieren(self, i, data): #fügt nach dem index i ein
+        if self.__len__() < i:
+            raise IndexError
+        node_i = self._first_node
+        for _ in range(0, i):
+            node_i = node_i.next
+            
+        node_next = node_i.next
+        node_i.next = Node(data=data, next=node_next)
     
-    def deletieren():
-        pass
+    def deletieren(self, i): #löscht den index
+        if self.__len__() < i:
+            raise IndexError
+        node_i = self._first_node
+        for _ in range(0, i-1):
+            node_i = node_i.next
+            
+        node_next = node_i.next.next
+        node_i.next = node_next
     
-    def pop():
-        pass
+    def pop(self):
+        current = self._first_node
+        while current.next.next != None:
+            current = current.next
+        
+        data = current.next
+        current.next = None
+        return data
 
     def __len__(self):
         count = 0
-        while next != None:
+        current = self._first_node
+        while current.next != None:
+            current = current.next
             count += 1
+        return count
 
     def all(self):
         all = list()
-        next = self.first
+        next = self._first_node
         while next != None:
             all.append(next.data)
             next = next.next
         return all
     
-    def last (self):
+    def last(self):
         all = list()
-        next = self.first
+        next = self._first_node
         while next.next != None:
             next = next.next
         return next.data
+    
+    def first(self):
+        return self._first_node.data
+    
--- Verkettete_Liste/verlis.py
+++ Verkettete_Liste/verlis.py
@@ -5,21 +5,23 @@ import classes
 def main():
     listn = classes.LinkedList(1)
     
-    listn.apendieren(2)
-    listn.apendieren(3)
-    listn.apendieren("test")
+    listn.__add__(2)
+    listn.__add__(3)
+    listn.__add__("test")
     
-    x = 0
     for l in listn:
         print(l)
-        x += 1
-        if x >= 10:
-            continue
-        
+    print("")
     
-    print(listn.first)
-    print(listn.last())
-    print(listn.all())
+    listn.insertieren(1, "insert")
+    listn.deletieren(3)
+    
+    print("last= ", listn.last())
+    print("first= ", listn.first())
+    print("last= ", listn.last())
+    print("all=", listn.all())
+    print("pop=", listn.pop())
+    print("all= ", listn.all())
     
  
 if __name__ == "__main__":
